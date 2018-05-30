bbcb8e9d9c134287c4178fc354f1173f2b053919
Carrie Savinell <carrielane89@gmail.com>
2018-05-30 15:20:31 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -23,7 +23,7 @@ class Triangle
   def valid_triangle
     valid = true 
     inequality_triangle = [(x + y > z), (y + z > x), (z + x > y)]
-    valid = false if inequality_triangle.any? {|triangle| triangle != triangle } 
+    valid = false if inequality_triangle.any? {|triangle| != triangle  } 
     #binding.pry
     sides = [x, y, z]
     binding.pry
